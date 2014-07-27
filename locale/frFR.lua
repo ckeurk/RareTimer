ba5845bbb4049372dc438315739c6d83772690ac
Kowh <bob913@sandwich.net>
2014-07-27 11:59:16 -0400

    French translations (thanks Leosky)

--- locale/frFR.lua
+++ locale/frFR.lua
@@ -1,5 +1,15 @@
 local L = Apollo.GetPackage("Gemini:Locale-1.0").tPackage:NewLocale("RareTimer", "frFR",false)
 if not L then return end
-
-L["Scorchwing"] = "Scorchwing"
+ 
+-- State strings
+L["StateUnknown"] = 'Inconnu'
+L["StateKilled"] = 'Tué à %s'
+L["StateDead"] = 'Tué au plus tard à %s'
+L["StatePending"] = 'Devrait reparaitre avant %s'
+L["StateAlive"] = 'En vie'
+L["StateInCombat"] = 'En combat'
+L["StateExpired"] = 'Inconnu (vu la dernière fois à %s)'
+ 
+-- Mob names
+L["Scorchwing"] = 'Ailardente'
 
