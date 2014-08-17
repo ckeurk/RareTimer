0fb5de41e8812e2fdc8b753421e05fa473721731
Kowh <bob913@sandwich.net>
2014-08-16 21:50:03 -0400

    Add Toggle

--- RareTimer.lua
+++ RareTimer.lua
@@ -190,8 +190,7 @@ function RareTimer:OnRareTimerOn(sCmd, sInput)
         elseif options.hide then
             self.wndMain:Show(false)
         elseif options.toggle then
-            self:CPrint("Not yet implemented")
-            --self.wndMain:Toggle()
+            self.wndMain:Show(not self.wndMain:IsVisible())
         elseif options.reset then
             self:CPrint("Resetting RareTimer db")
             self.db:ResetProfile()
@@ -209,7 +208,6 @@ function RareTimer:OnRareTimerOn(sCmd, sInput)
 
             self:SendState(entry, nil, true)
             --]]
-            self:InitMainWindow()
         else
             self.opt.print_help()
         end
