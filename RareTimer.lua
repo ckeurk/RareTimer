dcdb2c822935f926708bba913c3696ce84626cd1
Kowh <bob913@sandwich.net>
2014-10-20 19:56:04 -0400

    Version increment

--- RareTimer.lua
+++ RareTimer.lua
@@ -20,7 +20,7 @@ local ICCommLib = ICCommLib
 -----------------------------------------------------------------------------------------------
 -- Constants
 -----------------------------------------------------------------------------------------------
-local MAJOR, MINOR = "RareTimer-0.1", 17
+local MAJOR, MINOR = "RareTimer-0.1", 18
 
 local DEBUG = false -- Debug mode
 local NONET = false -- Block send/receive data
