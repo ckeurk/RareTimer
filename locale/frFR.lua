bd46f4265715e50b257e477d97f3b528134e103c
ckeurk <c.keurk@gmail.com>
2015-11-26 11:22:21 +0100

    Update frFR.lua

--- locale/frFR.lua
+++ locale/frFR.lua
@@ -8,18 +8,18 @@ if not L then return end
 L["LocaleName"] = Locale
 
 --Heading strings
-L["CmdListHeading"] = "RareTimer registre d'état:" 
+L["CmdListHeading"] = "RareTimer registre d\194\180 \195\169tat:" 
 L["AlertHeading"] = "Alerte de RareTimer:"
 L["Name"] = "Nom"
 L["Status"] = "Condition"
-L["Last kill"] = "Tué"
+L["Last kill"] = "Tu\195\169"
 L["Health"] = "Vie"
 
 --Msgs
 L["NewVersionMsg"] = "Une nouvelle version de RareTimer est disponible."
-L["ObsoleteVersionMsg"] = "RareTimer n'est pas à jour et ne recevra plus de mises à jour d'autres clients."
+L["ObsoleteVersionMsg"] = "RareTimer n'est pas \195\160 jour et ne recevra plus de mises \195\160 jour d'autres clients."
 L["SnoozeMsg"] = "RareTimer: N'alerter pas pour %s minutes."
-L["SnoozeResetMsg"] = "RareTimer: Rappel d'alarme remise à zéro."
+L["SnoozeResetMsg"] = "RareTimer: Rappel d'alarme remise \195\160 z\195\169ro."
 L["Y"] = "O" -- Yes
 L["N"] = "N" -- No
 
@@ -27,14 +27,14 @@ L["N"] = "N" -- No
 L["Snooze"] = "Rappel"
 
 --Option strings
-L["OptSnoozeTimeout"] = "Durée du rappel d'alarme (minutes)"
-L["OptSnoozeTimeoutDesc"] = "Durée pendant qu'on n'alerte pas apres rappel d'alarm."
-L["OptSnoozeReset"] = "Remiser à zéro le rappel d'alarm"
-L["OptSnoozeResetDesc"] = "Remiser à zéro le rappel d'alarm"
-L["OptTargetTimeout"] = "N'alerter pas après avoir ciblé (minutes)"
-L["OptTargetTimeoutDesc"] = "N'alerter pas si on a ciblé l'ennemi dans le délai."
+L["OptSnoozeTimeout"] = "Dur\195\169e du rappel d'alarme (minutes)"
+L["OptSnoozeTimeoutDesc"] = "Dur\195\169e pendant qu'on n'alerte pas apres rappel d'alarm."
+L["OptSnoozeReset"] = "Remise \195\160 z\195\169ro de l'alarme"
+L["OptSnoozeResetDesc"] = "Remise \195\160 zÃ¨ro de l'alarme"
+L["OptTargetTimeout"] = "N'alerter pas apr\195\169s avoir cibl\195\169 (minutes)"
+L["OptTargetTimeoutDesc"] = "N'alerter pas si vous avez cibl\195\169 l'ennemi dans le dÃ©lai."
 L["OptPlaySound"] = "Faire sonner"
-L["OptPlaySoundDesc"] = "Faire sonner lorsque l'alerte est déclencher."
+L["OptPlaySoundDesc"] = "Faire sonner lorsque l'alerte est d\195\169clencher."
 
 --Time strings
 L["s"] = "s" -- Seconds
@@ -44,35 +44,35 @@ L["d"] = "j" -- Days
 
 -- State strings
 L["StateUnknown"] = 'Inconnu'
-L["StateKilled"] = 'Tué à %s'
-L["StateDead"] = 'Tué au plus tard à %s'
+L["StateKilled"] = 'Tu\195\169 â€¡ %s'
+L["StateDead"] = 'Tu\195\169 au plus tard â€¡ %s'
 L["StatePending"] = 'Devrait reparaitre avant %s'
 L["StateAlive"] = 'En vie (%s)'
 L["StateInCombat"] = 'En combat'
-L["StateExpired"] = 'Inconnu (vu la dernière fois à %s)'
-L["StateTimerSoon"] = 'Bientôt (%s)'
-L["StateTimerTick"] = 'Commencé à %s'
-L["StateTimerRunning"] = 'Prochain à %s'
+L["StateExpired"] = 'Inconnu (vu la derni\195\169re fois â€¡ %s)'
+L["StateTimerSoon"] = 'Bient\195\180t (%s)'
+L["StateTimerTick"] = 'Commence dans %s'
+L["StateTimerRunning"] = 'Prochain dans %s'
  
 -- Mob names
-L["Aggregor the Dust Eater"] = "Aggregor le Mâchepoussière"
+L["Aggregor the Dust Eater"] = "Aggregor le Mâ€šchepoussi\195\169re"
 L["Bugwit"] = "Bugwit"
 L["Critical Containment"] = "Tissu d'horreurs"
-L["Defensive Protocol Unit"] = "Unité de protocoles défensifs"
-L["Doomthorn the Ancient"] = "Funestépine l'Ancien"
+L["Defensive Protocol Unit"] = "Unit\195\169 de protocoles d\195\169fensifs"
+L["Doomthorn the Ancient"] = "Funest\195\169pine l'Ancien"
 L["Gargantua"] = "Gargantua"
 L["Grendelus the Guardian"] = "Gardien Grendelus"
 L["Grinder"] = "Broyeur" 
 L["Hoarding Stemdragon"] = "Plandragon avide"
 L["KE-27 Sentinel"] = "Sentinelle KE-27"
 L["King Honeygrave"] = "Roi Nectaruine"
-L["Kraggar the Earth-Render"] = "Kraggar le Crève-terre"
+L["Kraggar the Earth-Render"] = "Kraggar le Cr\195\169ve-terre"
 L["Metal Maw"] = "Gueule d'acier"
 L["Metal Maw Prime"] = "Primo Gueule d'acier"
 L["Scorchwing"] = 'Ailardente'
-L["Subject J - Fiend"] = "Sujet J : démon"
+L["Subject J - Fiend"] = "Sujet J : d\195\169mon"
 L["Subject K - Brute"] = "Sujet K : brute"
 L["Subject Tau"] = "Sujet Tau"
-L["Subject V - Tempest"] = "Sujet V : tempête"
+L["Subject V - Tempest"] = "Sujet V : temp\195\169te"
 L["Zoetic"] = "Zoetic"
 L["Star-Comm Basin"] = "Star-Comm Basin"
